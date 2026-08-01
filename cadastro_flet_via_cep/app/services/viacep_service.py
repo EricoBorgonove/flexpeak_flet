4bda72435184931992f4c24a3f3dd104b215ff2e
ericoborgonove <borgonove@gmail.com>
2026-08-01 10:01:35 -0400

    feat (excel_repo): envio do arquivo completo

--- cadastro_flet_via_cep/app/services/viacep_service.py
+++ cadastro_flet_via_cep/app/services/viacep_service.py
@@ -1,3 +1,22 @@
 import requests
 from app.utils.strings import only_digits
 
+def fetch_address_from_viacep(cep: str) -> dict:
+    cep = only_digits(cep)
+    if len(cep) != 8:
+        raise ValueError("CEP deve ter 8 dígitos.")
+    
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    resp = requests.get(url, timeout=8)
+    resp.raise_for_status()
+    
+    data = resp.json()
+    if data.get("error") is True:
+        raise ValueError ("CEP não encontrado no Via CEP")
+    
+    return {
+        "logradouro": data.get("logradouro", "") or "",
+        "bairro":data.get("bairro", "") or "",
+        "cidade":data.get("localidade", "") or "",
+        "uf":data.get("uf", "") or ""
+    }
