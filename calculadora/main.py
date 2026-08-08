612f014bd4da1734b4ad7e49f228d5946b8b4d09
ericoborgonove <borgonove@gmail.com>
2026-08-08 10:50:32 -0400

    ajuste na calculadora de identacao

--- calculadora/main.py
+++ calculadora/main.py
@@ -4,7 +4,7 @@ import flet as ft
 def main (page: ft.Page):
     #Configurações da página
     page.title = "Calculadora"
-    page.padding = 30
+    page.padding = 5
     page.theme_mode = ft.ThemeMode.LIGHT
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -20,7 +20,7 @@ def main (page: ft.Page):
     )
     # Função executada ao clicar em numeros ou operaçoes
     def clicar (e):
-        valor = e.control.text
+        valor = e.control.content
         
         #se estiver mostrando erro, limpa antes de continuar
         if visor.value == "Erro":
@@ -40,7 +40,7 @@ def main (page: ft.Page):
         page.update()
         
     #Calcular o resultado
-    def calcular (e):
+    def calcular(e):
         try:
             expressao = visor.value
         
@@ -61,66 +61,66 @@ def main (page: ft.Page):
         page.update()
         
         #Função para criar botoes
-        def criar_botao (texto, funcao=clicar):
-            return ft.ElevatedButton(
-                text = texto,
-                width= 70,
-                height= 60,
-                on_click= funcao
-            )
-        
-        #Primerira linha
-        linha1 = ft.Row(
-            controls =[
-                criar_botao("C", limpar),
-                criar_botao("←", apagar),
-                criar_botao("÷"),
-                criar_botao("x"),
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
+    def criar_botao (texto, funcao=clicar):
+        return ft.ElevatedButton(
+            content = texto,
+            width= 70,
+            height= 60,
+            on_click= funcao
         )
-        #Segunda linha
-        linha2 = ft.Row(
-            controls =[
-                criar_botao("7"),
-                criar_botao("8"),
-                criar_botao("9"),
-                criar_botao("-"),
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
-        )        
-        #Terceira linha
-        linha3 = ft.Row(
-            controls =[
-                criar_botao("4"),
-                criar_botao("5"),
-                criar_botao("6"),
-                criar_botao("+"),
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
-        )           
-        #Quarta linha
-        linha4 = ft.Row(
-            controls =[
-                criar_botao("1"),
-                criar_botao("2"),
-                criar_botao("3"),
-                criar_botao("="),
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
-        )   
-        #Quinta linha
-        linha5 = ft.Row(
-            controls =[
-                criar_botao("0"),
-                criar_botao("."),
-                criar_botao("("),
-                criar_botao(")"),
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
-        )   
+    
+    #Primerira linha
+    linha1 = ft.Row(
+        controls =[
+            criar_botao("C", limpar),
+            criar_botao("←", apagar),
+            criar_botao("÷"),
+            criar_botao("x"),
+        ],
+        alignment = ft.MainAxisAlignment.CENTER
+    )
+    #Segunda linha
+    linha2 = ft.Row(
+        controls =[
+            criar_botao("7"),
+            criar_botao("8"),
+            criar_botao("9"),
+            criar_botao("-"),
+        ],
+        alignment = ft.MainAxisAlignment.CENTER
+    )        
+    #Terceira linha
+    linha3 = ft.Row(
+        controls =[
+            criar_botao("4"),
+            criar_botao("5"),
+            criar_botao("6"),
+            criar_botao("+"),
+        ],
+        alignment = ft.MainAxisAlignment.CENTER
+    )           
+    #Quarta linha
+    linha4 = ft.Row(
+        controls =[
+            criar_botao("1"),
+            criar_botao("2"),
+            criar_botao("3"),
+            criar_botao("=", calcular),
+        ],
+        alignment = ft.MainAxisAlignment.CENTER
+    )   
+    #Quinta linha
+    linha5 = ft.Row(
+        controls =[
+            criar_botao("0"),
+            criar_botao("."),
+            criar_botao("("),
+            criar_botao(")"),
+        ],
+        alignment = ft.MainAxisAlignment.CENTER
+    )   
         # Adiciona tudo a página
-        page.add(visor,linha1,linha2,linha3,linha4,linha5)
+    page.add(visor,linha1,linha2,linha3,linha4,linha5)
         
 if __name__ == "__main__":
-    ft.run(main)
+    ft.run(main)       
